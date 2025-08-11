96188b4e40ee89a463476d3fc0556f6a5a24591c
24LP9763 24LP9763 <24LP9763@G304-CC0217.localdomain>
2025-08-11 15:29:35 +0800

    fixed width ad

--- food/src/app/(admin)/admin/_components/FoodCard.tsx
+++ food/src/app/(admin)/admin/_components/FoodCard.tsx
@@ -14,7 +14,7 @@ export const FoodCard = ({ food, categoryId, getFoods }: FoodCardProps) => {
       <CardContent className="p-3 sm:p-4 rounded-[20px] flex flex-col gap-2 relative">
         <img
           src={food?.image}
-          className="h-[50%] sm:h-[129px] w-full object-cover rounded-xl"
+          className="h-[200px] sm:h-[129px] w-full object-cover rounded-xl"
         />
         <EditFood food={food} categoryId={categoryId} getFoods={getFoods} />
         <div className="flex flex-col gap-2">

471fb82b291ae017e0dfa081e7b64b7a194e98a7
24LP9763 24LP9763 <24LP9763@G304-CC0217.localdomain>
2025-08-11 15:16:05 +0800

    fixed width gg

--- food/src/app/(customer)/_components/Foods.tsx
+++ food/src/app/(customer)/_components/Foods.tsx
@@ -61,7 +61,7 @@ export const Foods = ({
           </button>
         </div>
       ) : foods.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 md:gap-[54px] mt-6 sm:mt-8 md:mt-[54px]">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 md:gap-10 mt-6 sm:mt-8 md:mt-[54px]">
           {foods.map((food) => (
             <FoodCard food={food} key={food._id} />
           ))}

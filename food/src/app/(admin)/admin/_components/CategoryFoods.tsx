83de67eb216e90ceae4492d41977856f4adbdffc
24LP9763 24LP9763 <24LP9763@G304-CC0217.localdomain>
2025-08-11 15:39:33 +0800

    fixed width f

--- food/src/app/(admin)/admin/_components/CategoryFoods.tsx
+++ food/src/app/(admin)/admin/_components/CategoryFoods.tsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from "react";
 import { FoodCard } from "./FoodCard";
 import { AddFood } from "./AddFood";
 import { api } from "@/axios";
+
 type CategoryFoodsProps = {
   categoryId: string;
   categoryName: string;
@@ -25,7 +26,6 @@ export const CategoryFoods = ({
   const getFoods = useCallback(async () => {
     try {
       const { data } = await api.get(`/food?categoryId=${categoryId}`);
-
       setFoods(data.food);
     } catch (error) {
       console.error("Error fetching food:", error);
@@ -39,10 +39,10 @@ export const CategoryFoods = ({
 
   return (
     <div className="bg-white p-3 sm:p-4 lg:p-5 rounded-xl flex flex-col gap-3 sm:gap-4">
-      <h4 className="text-lg sm:text-xl font-semibold">
+      <h4 className="text-lg sm:text-xl lg:text-2xl font-semibold">
         {categoryName} ({foods.length})
       </h4>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-3 sm:gap-4 lg:gap-5 justify-items-center">
         <AddFood
           getFoods={getFoods}
           categoryId={categoryId}
